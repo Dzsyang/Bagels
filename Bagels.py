fee8d4dff9d61f9fe409894db0d83d8fb5de54b0
linzixiang <linzixiang@sjtu.edu.cn>
2022-12-26 18:58:56 +0800

    Update

--- Bagels.py
+++ Bagels.py
@@ -4,13 +4,31 @@ import sys
 import os
 
 if sys.platform == "win32":
+    import msvcrt
     def Clear():
         os.system("cls")
-        return
+    def press_any_key(msg):
+        print(msg)
+        msvcrt.getch()
 else:
+    import termios
     def Clear():
         os.system(r"clear")
-        return
+    def press_any_key(msg):
+        fd = sys.stdin.fileno()
+        old_ttyinfo = termios.tcgetattr(fd)
+        new_ttyinfo = old_ttyinfo[:]
+        new_ttyinfo[3] &= ~termios.ICANON
+        new_ttyinfo[3] &= ~termios.ECHO
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+        termios.tcsetattr(fd, termios.TCSANOW, new_ttyinfo)
+        os.read(fd, 7)
+        termios.tcsetattr(fd, termios.TCSANOW, old_ttyinfo)
+
+def Invalid():
+    print("Invalid input! Please enter again.")
+    return
 
 def drawanime():
     Clear()
@@ -37,10 +55,10 @@ def drawanime():
     Clear()
     return
 
-def Interface():
+def MainInterface(paras):
     Clear()
     print("***** B A G E L S *****")
-    print("Welcome to Bagels!")
+    print("Welcome to Bagels, " + paras[2][0] + "!")
     print("\nPlease read Readme.md file before playing.\n")
     print("--START--       Enter 'p' to play.")
     print("--SETTING--     Enter 's' to set parameters.")
@@ -51,7 +69,7 @@ def Interface():
         x = input()
         y = 0
         if x not in command:
-            print("Invalid input! Please enter again.")
+            Invalid()
         else:
             if x == 'p' or x == 'P':
                 y = 1    
@@ -65,14 +83,77 @@ def Interface():
     Clear()
     return y
 
+def SettingInterface(paras):
+    Clear()
+    print("***** B A G E L S *****")
+    print("*** Setting ***")
+    print("Current parameters: " + "Digits = " + str(paras[0]) + ", Bots = " + str(paras[1]) + ".")
+    print("Notice:\nDue to technical limitations, the current parameters will not change immediately.\nYou can enter the setting interface again after exiting to view the current parameters.\n")
+    print("--DIGITS--     Enter 'd x' to set x digits of the number. (Default: 3)")
+    print("--BOTS--       Enter 'b x' to set x bots to play with you. (Default: 0)")
+    print("--RESTORE--    Enter 'r' to restore default values.")
+    print("--QUIT--       Enter 'q' to quit.")
+    command = ['d', 'D', 'b', 'B', 'r', 'R', 'q', 'Q']
+    while True:
+        x = input()
+        if x[0] not in command or x == "":
+            Invalid()
+        elif x[0] == 'q' or x[0] == 'Q':
+            Clear()
+            return paras
+        elif x[0] == 'r' or x[0] == 'R':
+            paras[0] = 3
+            paras[1] = 0
+            print("Suceessful!")
+        else:
+            s = x[2:]
+            if s.isdigit():
+                if x[0] == 'd' or x[0] == 'D':
+                    if int(s) == 0:
+                        Invalid()
+                    else:
+                        paras[0] = int(s)
+                        print("Successful!")
+                elif x[0] == 'b' or x[0] == 'B':
+                    paras[1] = int(s)
+                    print("Successful!")
+            else:
+                Invalid()
+
 def Initialize():
+    random.seed(time.time())
+    print("What's your name?")
+    players = [input()]
+    botrepo = ["Suzumiya Haruhi", "Asuna", "Index", "Misaka Mikoto", "Senjougahara Hitagi", "Saber", \
+               "Ayachi Nene", "Naruse Shiroha", "Kushima Kamome", "Natsume Ai", "Minakami Yuki"]
+    paras = [3, 0, players, botrepo]
+    Clear()
+    return paras
+
+def play(paras):
+    while True:
+        Is_ending = False
+        num = random.randint(10 ** (paras[0] - 1), 10 ** paras[0])
+        print(num)
+        x = int(input())
+        if x == num:
+            Is_ending = True
+        if Is_ending:
+            print("Do you want to play again? [Y/n]", end = " ")
+            ans = input()
+            yes = ["Y", "y", "yes", "Yes"]
+            if ans in yes:
+                print("Ok, let's play again!")
+                press_any_key("Press any key to continue...")
+                Clear()
+            else:
+                break 
     return
 
-def play():
-    return
-
-def setting():
-    return
+def setting(paras):
+    while True:
+        paras = SettingInterface(paras)
+        return
 
 def help():
     return
@@ -84,13 +165,13 @@ def quit():
 
 # main
 drawanime()
-Initialize()
+paras = Initialize()
 while True:
-    mode = Interface()
+    mode = MainInterface(paras)
     if mode == 1:
-        play()
+        play(paras)
     elif mode == 2:
-        setting()
+        setting(paras)
     elif mode == 3:
         help()
     elif mode == 0:
